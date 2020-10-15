a95cd458c3f6b39eaea61fd2691e43db44a24139
pratyaysaha <pratyaysaha@outlook.com>
2020-10-15 23:13:08 +0530

    the minor bug

--- hellogithub.cpp
+++ hellogithub.cpp
@@ -3,5 +3,5 @@ using namespace std;
 int main()
 {
     cout<<"Hello GitHub !!"<<endl;
-    cout<<"i am going to making a minor change in the program";
+    cout<<"i am going to making a minor change in the program"<<endl;
 }
