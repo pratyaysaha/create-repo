46173f60385d9d020f516a138182df3116622fd7
pratyaysaha <pratyaysaha@outlook.com>
2020-10-16 02:53:12 +0530

    just nothing

--- hellogithub.cpp
+++ hellogithub.cpp
@@ -13,9 +13,7 @@ int main()
     cout<<"i am going to making a minor change in the program"<<endl;
     cout<<"BLAHHHHH"<<endl;
     add();
-<<<<<<< HEAD
     cout<<"Addition is a nice way of adding";
-=======
     cout<<"The world is a better place";
->>>>>>> master
+
 }
